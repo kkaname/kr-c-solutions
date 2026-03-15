040ac09e723d62f2f00bc44e7b5dbda1e6e592ee
Ayush Hegde <ayushhegde2005@gmail.com>
2026-03-16 05:11:17 +0530

    removed the comment of the question as it is already given in the readme file
    Signed-off-by: Ayush Hegde <ayushhegde2005@gmail.com>

--- chapter_1/exercise_1_21/entab.c
+++ chapter_1/exercise_1_21/entab.c
@@ -1,11 +1,9 @@
 #include <stdio.h>
 
-/* Exercise 1.21: Write a program entab that replaces strings of blanks by the minimum
- * number of tabs and blanks to achieve the same spacing. Use the same tab stops as for detab.
- * When either a tab or a single blank would suffice to reach a tab stop, which should be given preference?
+/*
+ * Question: When either a tab or a single blank would suffice to reach a tab stop, which should be given preference?
+ * Answer: When a tab would suffice, it should be given preference.
  */
-
- /* When a tab would suffice, it should be given preference. */
 
 #define TAB_WIDTH 8     //defines the width of the tab character
 
