d119450eda7c0c61f4dd3f2289350065d2a9f327
Ayush Hegde <ayushhegde2005@gmail.com>
2026-03-16 05:06:59 +0530

    refactor code to follow same code format and removed exercise question as comment
    Signed-off-by: Ayush Hegde <ayushhegde2005@gmail.com>

--- chapter_1/exercise_1_20/detab.c
+++ chapter_1/exercise_1_20/detab.c
@@ -1,9 +1,5 @@
 #include <stdio.h>
 
-/* Exercise 1.20: Write a program detab that replaces tabs in the input with the proper number of blanks
- * to space to the next tab stop. Assume a fixed set of tab stops, say every n columns.
- * Should n be a variable or a symbolic parameter. */
-
 #define TAB_WIDTH 8
 
 int main(void) {
@@ -12,10 +8,10 @@ int main(void) {
 
     tab_stop = 1;
     current_col = 0;
-    while((c = getchar()) != EOF) {
+    while ((c = getchar()) != EOF) {
         if (c == '\t') {
             no_spaces = TAB_WIDTH - (current_col % TAB_WIDTH);
-            while(no_spaces){
+            while (no_spaces){
                 putchar(' ');
                 --no_spaces;
                 ++current_col;
@@ -30,6 +26,6 @@ int main(void) {
             }
         }
     }
-    
+
     return 0;
 }
