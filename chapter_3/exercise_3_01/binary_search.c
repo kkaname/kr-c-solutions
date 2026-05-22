731f1cfb90ce692920a71fc42c183d0c4bff745f
kkaname <ayushhegde2005@gmail.com>
2026-05-22 20:35:02 +0530

    exercise 3-1 solution

--- chapter_3/exercise_3_01/binary_search.c
+++ chapter_3/exercise_3_01/binary_search.c
@@ -1,41 +1,84 @@
 #include <stdio.h>
+#include <time.h>
 
-int binary_search(int *arr, int num, int n) {
-    int low = 0;
-    int high = n - 1;
-    int mid;
+#define MAXLEN 100
+#define ITERATIONS 100
 
-    while (low < high) {
-        mid = (low + high)/2;
-        printf("**********\n");
-        printf("low = %d, high = %d, mid = %d\n", low, high, mid);
-        printf("**********\n");
-        if (num <= arr[mid]) {
-            high = mid;
+long test_binary_search(int (*binarysearch)(int v[], int n, int x), int v[], int n, int x);
+int actual_binary_search(int v[], int n, int x);
+int binary_search(int v[], int n, int x);
+
+int main(void) {
+    int v[MAXLEN];
+    int ele;
+
+    for (int i = 0; i < MAXLEN; i++) {
+        v[i] = i + 1;
+    }
+
+    ele = -1;
+
+    long clock_count;
+
+    // first we test the Binary Search with the if-else if inside the loop
+    clock_count = test_binary_search(actual_binary_search, v, MAXLEN, ele);
+    printf("Binary Search with if-else if: %lu clock_count (%f seconds)\n", clock_count, (double)clock_count / CLOCKS_PER_SEC);
+
+
+    // then we test the Binary Search with only one test inside 
+    clock_count = test_binary_search(binary_search, v, MAXLEN, ele);
+    printf("Binary Search with only one if: %lu clock_count (%f seconds)\n", clock_count, (double)clock_count / CLOCKS_PER_SEC);
+
+    return 0;
+}
+
+int actual_binary_search(int v[], int n, int x) {
+    int low, high, mid; 
+    low = 0;
+    high = n - 1;
+
+    while (low <= high) {
+        mid = (low + high) / 2;
+        if (v[mid] == x) {
+            return mid;
+        }
+        else if (x < v[mid]) {
+            high = mid - 1;
         }
         else {
             low = mid + 1;
         }
     }
-    if (arr[low] == num) {
-        return low;
-    }
     return -1;
 }
 
+int binary_search(int v[], int n, int x) {
+    int low, high, mid; 
+    low = 0;
+    high = n - 1;
 
-int main(void) {
-    int arr[20] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20};
-    int num = 12;
-
-    int result = binary_search(arr, num, 20);
-
-    if (result == -1) {
-        printf("Element Not Found.\n");
+    while (low <= high) {
+        mid = (low + high) / 2;
+        if (x < v[mid]) {
+            high = mid - 1;
+        }
+        else {
+            low = mid + 1;
+        }
     }
-    else {
-        printf("Element %d is in as the location: %d\n", num, result);
-    }
 
-    return 0;
+    if (high >= 0 && x == v[high - 1]) {
+        return high;
+    }
 }
+
+long test_binary_search(int (*binarysearch)(int v[], int n, int x), int v[], int n, int x) {
+    long clock_count = clock();
+
+    for (int i = 0; i < ITERATIONS; i++) {
+        binarysearch(v, n, x);
+    }
+    
+    return clock() - clock_count;
+}
+
