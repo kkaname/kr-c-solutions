2f261e5f88282ab5a252971d530190e132ba97ec
kkaname <ayushhegde2005@gmail.com>
2026-05-26 04:24:38 +0530

    refactor: modularized the code for better readability and clarity.

--- chapter_1/exercise_1_14/char_freq_histogram.c
+++ chapter_1/exercise_1_14/char_freq_histogram.c
@@ -1,15 +1,17 @@
 #include <stdio.h>
 
-#define SMALL_LETTER 26 //26 alphabets from a to z
-#define CAP_LETTER 26 //26 alphabets from a to z
-#define NUM 10 //10 decimal digit
-#define OTHERS 1
+#define LOWERCASE_COUNT 26 //26 alphabets from a to z
+#define UPPERCASE_COUNT 26 //26 alphabets from a to z
+#define DIGIT_COUNT 10 //10 decimal digit
+#define OTHERS_COUNT 1
+
+void print_histogram(int freq[]);
 
 int main(void) {
-    int c, freq[SMALL_LETTER + CAP_LETTER + NUM + OTHERS];
+    int c, freq[LOWERCASE_COUNT + UPPERCASE_COUNT + DIGIT_COUNT + OTHERS_COUNT];
     int i;
 
-    for (i = 0; i < (SMALL_LETTER + CAP_LETTER + NUM + OTHERS); i++) {
+    for (i = 0; i < (LOWERCASE_COUNT + UPPERCASE_COUNT + DIGIT_COUNT + OTHERS_COUNT); i++) {
         freq[i] = 0;
     }
 
@@ -18,29 +20,35 @@ int main(void) {
             ++freq[c - 97];
         }
         else if (c >= 'A' && c <= 'Z'){
-            ++freq[SMALL_LETTER + c - 65];
+            ++freq[LOWERCASE_COUNT + c - 65];
         }
         else if (c >= '0' && c <= '9') {
-            ++freq[SMALL_LETTER + CAP_LETTER + c - '0'];
+            ++freq[LOWERCASE_COUNT + UPPERCASE_COUNT + c - '0'];
         }
         else {
-           ++freq[SMALL_LETTER + CAP_LETTER + NUM];
+           ++freq[LOWERCASE_COUNT + UPPERCASE_COUNT + DIGIT_COUNT];
         }
     }
 
     // Now we print the histogram
+    print_histogram(freq);
+
+    return 0;
+}
+
+void print_histogram(int freq[]) {
     printf("Word Frequency Histogram:\n");
     printf("-------------------------\n");
-    for (i = 0; i < (SMALL_LETTER + CAP_LETTER + NUM + OTHERS); i++) {
+    for (int i = 0; i < (LOWERCASE_COUNT + UPPERCASE_COUNT + DIGIT_COUNT + OTHERS_COUNT); i++) {
         if (freq[i] > 0) {
-            if (i < SMALL_LETTER) {
+            if (i < LOWERCASE_COUNT) {
                 printf("%c: ", 'a' + i);
             }
-            else if (i < SMALL_LETTER + CAP_LETTER) {
-                printf("%c: ", 'A' + (i - SMALL_LETTER));
+            else if (i < LOWERCASE_COUNT + UPPERCASE_COUNT) {
+                printf("%c: ", 'A' + (i - LOWERCASE_COUNT));
             }
-            else if (i < (SMALL_LETTER + CAP_LETTER + NUM)) {
-                printf("%c: ", '0' + (i - SMALL_LETTER - CAP_LETTER));
+            else if (i < (LOWERCASE_COUNT + UPPERCASE_COUNT + DIGIT_COUNT)) {
+                printf("%c: ", '0' + (i - LOWERCASE_COUNT - UPPERCASE_COUNT));
             }
             else {
                 printf("others:");
@@ -52,6 +60,4 @@ int main(void) {
             putchar('\n');
         }
     }
-
-    return 0;
 }
