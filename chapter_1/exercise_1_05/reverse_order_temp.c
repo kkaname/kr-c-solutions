2642a54f82107367e7ca4f9df44bd3469cf32e2d
kkaname <ayushhegde2005@gmail.com>
2026-05-25 17:03:23 +0530

    refractor: changed the while loop in the logic to for loop for conciseness

--- chapter_1/exercise_1_05/reverse_order_temp.c
+++ chapter_1/exercise_1_05/reverse_order_temp.c
@@ -11,18 +11,20 @@ int main(void) {
     printf("Celsius\t\tFahrenheit\n");
     printf("--------------------------\n");
 
-    celsius = upper;
-    while (celsius >= lower) {
-        fahr = (9.0 / 5.0) * celsius + 32.0;
+    for (celsius = upper; celsius >= lower; celsius -= step) {
+        fahr = (9.0 / 5.0) * celsius + 32.0f;
         printf("%3.0f\t\t%6.2f\n", celsius, fahr);
-        celsius -= step;
     }
 
-    /*Alternate way to do is by using for loop:
-     * >for (celsius = 300; celsius >= 0; celsius -= 20) {
-     * >    fahr = (9.0 / 5.0) * celsius - 32.0f;
-     * >    printf("%3.0f\t\t%6.2f\n", celsius, fahr);
-     * >}
+    /*
+     * Alternate way to do is by using while loop:
+     *
+     * celsius = upper;
+     * while (celsius >= lower) {
+     *   fahr = (9.0 / 5.0) * celsius + 32.0;
+     *   printf("%3.0f\t\t%6.2f\n", celsius, fahr);
+     *   celsius -= step;
+     * }
      */
 
     return 0;
