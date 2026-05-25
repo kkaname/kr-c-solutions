271c4726c9176414739f0f4c3db230fdba2afba1
kkaname <ayushhegde2005@gmail.com>
2026-05-26 02:48:29 +0530

    refactor: switched to switch statement in place of else if ladder and also added commented to improve readability. Also commented out the solution using else - if ladder as an alternate solution

--- chapter_1/exercise_1_10/char_replacement.c
+++ chapter_1/exercise_1_10/char_replacement.c
@@ -4,22 +4,48 @@ int main(void) {
     int c;
 
     while ((c = getchar()) != EOF) {
-        if (c == '\t') {
-            putchar('\\');
-            putchar('t');
-        }
-        else if (c == '\\') {
-            putchar('\\');
-            putchar('\\');
-        }
-        else if (c == '\b') {
-            putchar('\\');
-            putchar('b');
-        }
-        else {
-            putchar(c);
+        switch (c) {
+            case '\t':
+                putchar('\\');
+                putchar('t');
+                break;
+            case '\\':
+                putchar('\\');
+                putchar('\\');
+                break;
+            case '\b':
+                putchar('\\');
+                putchar('b');
+                break;
+            default:
+                putchar(c);
         }
     }
+    /*
+     * The reason I have prefered switch statement is that
+     * it is faster then if - else if ladder and better readability.
+     */
+
+    /* -> Alternate way to solve:
+     *
+     * while ((c = getchar()) != EOF) {
+     *   if (c == '\t') {
+     *       putchar('\\');
+     *       putchar('t');
+     *   }
+     *   else if (c == '\\') {
+     *       putchar('\\');
+     *       putchar('\\');
+     *   }
+     *   else if (c == '\b') {
+     *       putchar('\\');
+     *       putchar('b');
+     *   }
+     *   else {
+     *       putchar(c);
+     *   }
+     *}
+     */
 
     return 0;
 }
