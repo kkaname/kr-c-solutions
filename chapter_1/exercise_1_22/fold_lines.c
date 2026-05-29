6c8e2b032188060f1ca2e464fe459339a24b177a
kkaname <ayushhegde2005@gmail.com>
2026-05-30 02:06:37 +0530

    Add: exercise program 5

--- chapter_1/exercise_1_22/fold_lines.c
+++ chapter_1/exercise_1_22/fold_lines.c
@@ -1,11 +1,5 @@
 #include <stdio.h>
 
-/* Exercise 1.22: Write a program to "fold" long input lines into two or more shorter lines
- * after the last non-blank character that occurs before the n-th column of input.
- * Make sure your program does something intelligent with very long lines,
- * and if there are no blanks or tabs before the specified column, break after the specified column.
- */
-
 #define MAXLEN 200
 #define BREAKPOINT 40
 
